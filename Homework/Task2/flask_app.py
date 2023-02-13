fcd4fbffa5f57f3967ba6208d198dffee9294bf0
RaynGoslenko <olkhovatyi@knu.ua>
2023-02-13 16:14:11 +0200

    Homework 4

--- Homework/Task2/flask_app.py
+++ Homework/Task2/flask_app.py
@@ -1,6 +1,5 @@
 import time
 from flask import Flask, request, render_template
-from flask import jsonify
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.safari.options import Options
@@ -9,43 +8,37 @@ from django.core.validators import URLValidator
 app = Flask(__name__)
 
 
-@app.route('/ping', methods=['GET', 'POST'])
-def ping():
-    return jsonify(success=True)
-
-def parse(link):
-    options = Options()
-    options.add_argument("--headless")
-    driver = webdriver.Safari(options=options)
-
-    val = URLValidator()
-    try:
-        val(f'{link}')
-    except:
-        driver.quit()
-        return ""
-
-    driver.execute_script(f"window.location.href = '{link}'")
-
-    time.sleep(7)
-
-    page = driver.page_source
-    soup = BeautifulSoup(''.join(page), 'html.parser')
-
-    articles = soup.find_all('p')
-    articles_text = []
-
-    for article in articles:
-        articles_text.append(article.text)
-
-    driver.quit()
-    return articles_text
-
+@app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == "POST":
-        text = request.form.get("text_input")
+        link = request.form.get("text_input")
 
-        news_list = parse(text)
+        options = Options()
+        options.add_argument("--headless")
+        driver = webdriver.Safari(options=options, )
+
+        val = URLValidator()
+        try:
+            val(f'{link}')
+        except:
+            driver.quit()
+            return render_template("index.html", output="")
+
+        driver.execute_script(f"window.location.href = '{link}'")
+
+        time.sleep(7)
+
+        page = driver.page_source
+        soup = BeautifulSoup(''.join(page), 'html.parser')
+
+        articles = soup.find_all('p')
+        news_list = []
+
+        for article in articles:
+            news_list.append(article.text)
+
+        driver.quit()
+
         for article in news_list:
             print(article)
     else:
